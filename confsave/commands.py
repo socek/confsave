81f687c61519aa5a66772cf28cc21600ddd5c773
Dominik Dlugajczyk <d.dlugajczyk@clearcode.cc>
2017-05-03 17:13:16 +0200

    More tests for commands.

--- confsave/commands.py
+++ confsave/commands.py
@@ -9,16 +9,25 @@ class Commands(object):
         self.app = app
 
     def _init_repo(self):
+        """
+        Initialize the git repo if needed and read the confsave config.
+        """
         self.app.repo.init_git_repo()
         self.app.repo.read_config()
 
     def add(self, filename):
+        """
+        Add file to the repo and change it to the symlink.
+        """
         self._init_repo()
         endpoint = Endpoint(self.app, filename)
         endpoint.add_to_repo()
         self.app.repo.write_config()
 
     def show_list(self):
+        """
+        Show list of files from home which are not yet added to the repo.
+        """
         for filename in glob(self.app.get_home_path() + '/.*'):
             endpoint = Endpoint(self.app, filename)
             if not endpoint.is_link():
@@ -29,6 +38,9 @@ class Commands(object):
     #     pass
 
     def show_status(self):
+        """
+        Show status of tracked files.
+        """
         self._init_repo()
         status = self.app.repo.git.git.status('-s')
         for line in status.split('\n'):
--- confsave/tests/test_commands.py
+++ confsave/tests/test_commands.py
@@ -1,5 +1,8 @@
 from mock import MagicMock
+from mock import call
+from mock import patch
 from pytest import fixture
+from pytest import yield_fixture
 
 from confsave.commands import Commands
 
@@ -14,15 +17,72 @@ class TestCommands(object):
     def commands(self, app):
         return Commands(app)
 
-    def test_add(self, commands):
+    @yield_fixture
+    def minit_repo(self, commands):
+        with patch.object(commands, '_init_repo') as mock:
+            yield mock
+
+    @yield_fixture
+    def mendpoint(self):
+        with patch('confsave.commands.Endpoint') as mock:
+            yield mock
+
+    @yield_fixture
+    def mprint(self):
+        with patch('confsave.commands.print') as mock:
+            yield mock
+
+    @yield_fixture
+    def mglob(self):
+        with patch('confsave.commands.glob') as mock:
+            yield mock
+
+    def test_init_repo(self, commands, app):
         """
+        ._init_repo should initialize the git repo if needed and read the confsave config.
+        """
+        commands._init_repo()
+
+        app.repo.init_git_repo.assert_called_once_with()
+        app.repo.read_config.assert_called_once_with()
+
+    def test_add(self, commands, minit_repo, mendpoint, app):
+        """
+        .add should:
+        1. initalize the repo
+        2. create endpoint
+        3. add endpoint to the repo
+        4. write config
         """
         commands.add('filename')
 
-    def test_show_list(self, commands):
+        minit_repo.assert_called_once_with()  # 1
+        mendpoint.assert_called_once_with(app, 'filename')  # 2
+        mendpoint.return_value.add_to_repo.assert_called_once_with()  # 3
+        app.repo.write_config.assert_called_once_with()  # 4
+
+    def test_show_list_when_endpoint_is_a_link(self, commands, mglob, mendpoint, mprint, app):
         """
+        .show_list should not show files that are in the homedir but are also a symlink
         """
+        mglob.return_value = ['first']
+        mendpoint.return_value.is_link.return_value = True
+
         commands.show_list()
+
+        mendpoint.assert_called_once_with(app, 'first')
+        assert not mprint.called
+
+    def test_show_list_when_endpoint_is_not_a_link(self, commands, mglob, mendpoint, mprint, app):
+        """
+        .show_list should show files that are in the homedir but are not links
+        """
+        mglob.return_value = ['first']
+        mendpoint.return_value.is_link.return_value = False
+        commands.show_list()
+
+        mendpoint.assert_called_once_with(app, 'first')
+        mprint.assert_called_once_with(mendpoint.return_value.path)
 
     # TODO: will be implemented at 0.2v
     # def test_ignore(self, commands):
@@ -30,7 +90,39 @@ class TestCommands(object):
     #    """
     #    commands.ignore('filename')
 
-    def test_status(self, commands):
+    def test_status(self, commands, app, mprint, minit_repo):
         """
+        .show_status should:
+        1. initalize the repo
+        2. get status from git
+        3. print proper line
         """
+        app.repo.git.git.status.return_value = 'status1\nstatus2\n'
+        app.settings.CONFIG_FILENAME = 'config'
+
         commands.show_status()
+
+        minit_repo.assert_called_once_with()  # 1
+        app.repo.git.git.status.assert_called_once_with('-s')  # 2
+        assert mprint.call_args_list == [  # 3
+            call('status1'),
+            call('status2'),
+            call(''),
+        ]
+
+    def test_status_with_config(self, commands, app, mprint, minit_repo):
+        """
+        .show_status should not print the config file
+        """
+        app.repo.git.git.status.return_value = 'status1\nconfig\nstatus2\n'
+        app.settings.CONFIG_FILENAME = 'config'
+
+        commands.show_status()
+
+        minit_repo.assert_called_once_with()
+        app.repo.git.git.status.assert_called_once_with('-s')
+        assert mprint.call_args_list == [  # 3
+            call('status1'),
+            call('status2'),
+            call(''),
+        ]
